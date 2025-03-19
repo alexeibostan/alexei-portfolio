4d425e91ff8cf4e8137aa091d6591e07e4139ec5
alexeibostan <alexei.bostan7@gmail.com>
2025-03-19 23:41:20 +0000

    Deploying to gh-pages from @ alexeibostan/alexei-portfolio@12ddb8c5c88941c7e2c8293f3a621e73e336a554 🚀

---
+++ _next/static/chunks/app/page-9c91cbb04272bb01.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,501,974],{5852:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.bind(n,1757))}},e=>{var s=s=>e(e.s=s);e.O(0,[757,441,587,358],()=>s(5852)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/projects/page-c37b6772eb9f47e9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[893],{5480:(e,s,_)=>{Promise.resolve().then(_.bind(_,1757))}},e=>{var s=s=>e(e.s=s);e.O(0,[757,441,587,358],()=>s(5480)),_N_E=e.O()}]);
