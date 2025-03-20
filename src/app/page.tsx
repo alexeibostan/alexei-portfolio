31b3a163115dc8d2b7509b00765caa72022155a6
Alexei Bostan <alexei.bostan7@gmail.com>
2025-03-20 01:36:02 +0100

    Fix image and manifest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,7 @@ export default function Home() {
                   src="/profile.webp" 
                   alt="Alexei Bostan" 
                   fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   style={{ objectFit: 'cover' }}
                   priority
                   className="rounded-full"
