d58744a91f208eb6ce806589dcecec44f235f57c
alexeibostan <alexei.bostan7@gmail.com>
2025-03-20 00:25:47 +0000

    Deploying to gh-pages from @ alexeibostan/alexei-portfolio@3408c80104d10cb9b77499d43aaebc0db3c2f19c 🚀

---
+++ _next/static/chunks/app/page-987442042292ff50.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{4713:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.bind(n,9895))}},e=>{var s=s=>e(e.s=s);e.O(0,[512,895,441,587,358],()=>s(4713)),_N_E=e.O()}]);
