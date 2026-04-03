ab2e222c9220cbb569519f5d1fcff66e91189ee6
Alexei Bostan <alexei.bostan7@gmail.com>
2026-04-03 19:01:20 +0200

    chore: remove TDD from methodologies list

--- src/data/methodologies.ts
+++ src/data/methodologies.ts
@@ -3,7 +3,6 @@ import type { Methodology } from '@/types';
 export const methodologies: Methodology[] = [
   { name: "Agile/Scrum" },
   { name: "CI/CD" },
-  { name: "TDD" },
   { name: "Code Review" },
   { name: "Mentoring" },
   { name: "Design Systems" },
